1185237aee4c4f754c622d7a2842d3a35918c4ab
Shubhankar Dey sarkar <shubhankar.deysarkar@wipro.com>
2025-02-23 06:02:28 +0000

    change in service

--- srv/service.js
+++ srv/service.js
@@ -1,5 +1,7 @@
 const cds = require('@sap/cds');
 
 module.exports = cds.service.impl( async function(srv) {
-    
+    srv.on('printhelloworld', async req => {
+        console.log(req.data) //print the request coming from server
+    })
 })
