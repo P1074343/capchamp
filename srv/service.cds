6b819845ec5a1a27977311e055883fd512b5b461
Shubhankar Dey sarkar <shubhankar.deysarkar@wipro.com>
2025-02-24 18:01:05 +0000

    rest

--- srv/service.cds
+++ srv/service.cds
@@ -1,3 +1,11 @@
+@protocol: 'rest'
 service product {
     function printhelloworld (input: String) returns String;
+
+    function addition (num1: Integer, num2: Integer) returns Integer;
+
+@open
+    type object {};
+    // This function takes an integer parameter 'category' and returns an object.
+    function MyFunction(category: Integer) returns object;
 }
--- srv/service.js
+++ srv/service.js
@@ -5,4 +5,30 @@ module.exports = cds.service.impl( async function(srv) {
         console.log(req.data.input) //print the request coming from server
         return `${req.data.input} World!`
     })
+
+    srv.on('addition', async req => {
+        console.log(req.data)
+        let result = req.data.num1 + req.data.num2;   
+        return result;
+    })
+
+    srv.on('MyFunction', async req => {
+        let result = {};
+        if(req.data.category == 1) {
+            result.product = 'BMW',
+            result.price = "USD 1200",
+            result.location = 'Germany'
+        } else {
+            result.product = 'Audi',
+            result.stock = 120,
+            result.priceArray = [
+                {
+                    "Price": 47567,
+                    "Currency": "EUR",
+                    "Discount": "20 %"
+                }
+            ]
+        }console.log(result);
+        return result;
+    })
 })
